9c9bb04dfcd2ed069b0a95065e583af32f6b8106
dipstef <dipstef@gmail.com>
2014-06-13 00:04:32 +0100

    Organized attempts modules

--- keepon/up_and_running.py
+++ keepon/up_and_running.py
@@ -1,4 +1,4 @@
-from funlib.retry.sleep import sleep, increment_sleep
+from funlib.retry.sleep import sleep, incremental_sleep
 
 from .attempts import on_no_connection, on_unresolvable_host, on_timeouts, on_connection_refused, on_response_error, \
     on_server_error
@@ -9,10 +9,10 @@ from .requests import RetryRequests
 up_and_running = (
     on_no_connection(sleep(1)),
     on_unresolvable_host(sleep(1), retry=10),
-    on_timeouts(increment_sleep(1, to=10)),
-    on_connection_refused(increment_sleep(1, to=10)),
+    on_timeouts(incremental_sleep(1, to=10)),
+    on_connection_refused(incremental_sleep(1, to=10)),
     on_response_error(sleep(2), retry=10),
-    on_server_error(increment_sleep(10, to=30), retry=10),
+    on_server_error(incremental_sleep(10, to=30), retry=10),
 )
 
 client = RetryRequests(up_and_running)
