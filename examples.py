26eaad2658bfeb02a39a4ee8e9d5c203cf78a4ea
dipstef <dipstef@gmail.com>
2014-07-01 17:34:41 +0100

    removed example file

--- examples.py
+++
@@ -1,6 +0,0 @@
-from funlib.retry.sleep import sleep
-from keepon import on_no_connection, Keepon
-
-#client = Keepon(on_no_connection(sleep=sleep(1), retry=10))
-
-#client.get('http://www.google.com')
