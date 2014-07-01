52bcc0234e5c9dafdb82744fc93588c9253c0af7
dipstef <dipstef@gmail.com>
2014-07-01 17:30:08 +0100

    dammit module updated

--- keepon/dammit.py
+++ keepon/dammit.py
@@ -8,10 +8,10 @@ from .attempt import on_internal_server_error, on_no_connection, on_unresolvable
 
 dammit = (
     on_no_connection(sleep(1)),
-    on_internal_server_error(incremental_sleep(5, to=60)),
     on_unresolvable_host(sleep(1)),
     on_timeouts(incremental_sleep(1, to=10)),
     on_connection_refused(incremental_sleep(1, to=10)),
+    on_internal_server_error(incremental_sleep(5, to=60)),
     on_response_error(sleep(2), retry=10),
     on_server_error(incremental_sleep(10, to=30), retry=10),
 )
